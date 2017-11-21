b7d43a66990ca3c227c42b6538e9036211ad84d5
Assem Deghady <assem.deghady@gmail.com>
2017-11-21 16:49:44 +0200

    Init httparty connection

--- lib/questionpro_rails.rb
+++ lib/questionpro_rails.rb
@@ -1,7 +1,11 @@
+require 'httparty'
 require "questionpro_rails/configuration"
 require "questionpro_rails/version"
 
 module QuestionproRails
+  include HTTParty
+  base_uri 'www.questionpro.com'
+
   class << self
     attr_accessor :configuration
   end
@@ -17,4 +21,14 @@ module QuestionproRails
   def self.configure
     yield(configuration)
   end
+
+  def base_path(method_name)
+    "/a/api/#{method_name}&api_key=#{ self.configure.api_key }"
+  end
+
+  def surveys(options = {})
+    url = "#{ base_path('questionpro.survey.getAllSurveys') }"
+    self.class.get(url, options)['response']['surveys']
+  end  
+
 end
