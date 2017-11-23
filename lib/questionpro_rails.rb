3b462067e117636b3f9fd546cd19d7ea6e464653
Assem Deghady <assem.deghady@gmail.com>
2017-11-23 15:33:44 +0200

    Basic Calls

--- lib/questionpro_rails.rb
+++ lib/questionpro_rails.rb
@@ -4,7 +4,16 @@ require "questionpro_rails/version"
 
 module QuestionproRails
   include HTTParty
+  format :json
+
   base_uri 'www.questionpro.com'
+
+  attr_accessor :surveys, :status
+
+  def initialize(response)
+    @surveys = response['response']['surveys']
+    @status = response['response']['status']
+  end
 
   class << self
     attr_accessor :configuration
@@ -22,13 +31,26 @@ module QuestionproRails
     yield(configuration)
   end
 
-  def base_path(method_name)
-    "/a/api/#{method_name}&api_key=#{ self.configure.api_key }"
+  def self.api_key
+    QuestionproRails.configuration.api_key
   end
 
-  def surveys(options = {})
-    url = "#{ base_path('questionpro.survey.getAllSurveys') }"
-    self.class.get(url, options)['response']['surveys']
+  def self.base_path(method_name)
+    "/a/api/#{method_name}&api_key=#{ QuestionproRails.api_key }"
+  end
+
+  def self.surveys(options = {})
+    url = "#{ QuestionproRails.base_path('questionpro.survey.getAllSurveys') }"
+    response = get(url)
+    if response.success?
+      new(response)
+    else
+      raise response.response
+    end        
   end  
 
 end
+
+# QuestionproRails.configure do |config|
+#   config.api_key = "0f00a8a8-fa4e-4d1d-ac47-bdde52a95944"
+# end
